f56e367b1860bdbd85ea93639ae8a50d1bb5b5e1
Uday Punia <udaypunia9027@gmail.com>
2025-11-27 17:54:39 +0530

    starting the revision of functions

---
+++ JavaScript/Revision_Functions.js
@@ -0,0 +1,41 @@
+//Functions is the building block of any program by which we can divide our large program into smaller parts
+
+//There  are mainly four types of functions that are present 
+/*
+1.Functions with return value without parameter
+2.Function with return value with parameter
+3.Functions without return value with parameter
+4.Function without return value without parameter 
+*/
+
+// 1.Functions with return value without parameter
+// function fun(){
+//     let a=10
+//     let b=50
+//     return a+b;
+// }
+// console.log(fun())
+
+
+
+// 2.Functions with return value with parameter
+// function fun(a,b){
+//     return a+b;
+// }
+// console.log(fun(1,2))
+
+//3.Functions without return value and with parameter
+// function fun(a,b){
+//     console.log(a+b);
+// }
+// fun(4,5)
+
+//4.Functions without return value and without parameter
+// function fun(){
+//     let a=22;
+//     let b=55;
+//     console.log(a+b)
+// }
+// fun()
+
+//
