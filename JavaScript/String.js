70824ceb9c2cb0a97a13f5b0516773e5cae9b6c3
Uday Punia <udaypunia9027@gmail.com>
2025-11-28 22:01:07 +0530

    Starting String

---
+++ JavaScript/String.js
@@ -0,0 +1,37 @@
+var a="Uday Punia"
+var b='Haridwar Uttarakhand'
+var c= `Front end web development engineering`
+// console.log(a)
+// console.log(b)
+
+
+//Accessing String 
+//Using while loop
+// let i=0
+// while(i<a.length)
+// {
+//     console.log(a[i])
+//     i++
+// }
+
+//Using do while loop
+// let i=0;
+// do{
+// console.log(a[i])
+// i++
+// }while(i<a.length)
+
+//Using for loop
+// for(let i=0;i<a.length;i++){
+//     console.log(a[i])
+// }
+
+//Using the for of loop
+// for(i of a){
+//     console.log(i)
+// }
+
+//Using the for in loop
+// for(i in a){
+//     console.log(a[i])
+// }
