4b47c37c86ddd666f5272eabcec0c5009f08e9b8
Uday Punia <udaypunia9027@gmail.com>
2025-12-04 13:47:33 +0530

    Learning DOM

---
+++ JavaScript/oops.js
@@ -0,0 +1,18 @@
+//Inheritance : 
+
+// class Parent{
+//     constructor(a,b){
+//         this.a=a
+//         this.b=b
+//     }
+// }
+// class Add extends Parent{
+//     constructor(a,b){
+//         super(a,b);
+//     }
+//     display(){
+//         console.log(`${this.a} + ${this.b} = ${this.a + this.b}`)
+//     }
+// }
+// var sum=new Add(10,20)
+// sum.display()
